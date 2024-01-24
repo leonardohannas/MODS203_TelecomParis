bff4d36feaff237e833700a4bc362b1d54570d23
Gabriele Lorenzo <gabriele.lorenzo6@gmail.com>
2024-01-24 15:47:30 +0100

    analysis script updated

--- src/analysis.py
+++ src/analysis.py
@@ -2,6 +2,7 @@ import os
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
 PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,6 +26,10 @@ def salary():
             for region in sorted_regions
         ],
         labels=sorted_regions,
+        patch_artist=True,
+        medianprops={"linewidth": 1.5, "color": "red"},
+        boxprops={"facecolor": "lightblue"},
+        flierprops={"marker": "o", "markerfacecolor": "black", "markersize": 2},
     )
     plt.subplots_adjust(
         top=0.953, bottom=0.285, left=0.041, right=0.99, hspace=0.2, wspace=0.2
