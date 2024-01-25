68f05b725ffdc95be86a79b4897693fed18fc1c5
Gabriele Lorenzo <gabriele.lorenzo6@gmail.com>
2024-01-25 14:22:52 +0100

    subcat and subsubcat histogram script added

--- src/analysis.py
+++ src/analysis.py
@@ -36,7 +36,7 @@ def salary():
     plt.show()
 
 
-def nutri_histogram():
+def nutri_cat_histogram():
     data = pd.read_csv(PATH + "/data/filtered_magasin_product.csv")
 
     data[["category", "region_name", "nutri_score_num"]].isnull().sum()
@@ -76,9 +76,101 @@ def nutri_histogram():
     plt.show()
 
 
+def nutri_subcat_histogram():
+    data = pd.read_csv(PATH + "/data/filtered_magasin_product.csv")
+
+    data[["subcategory", "region_name", "nutri_score_num"]].isnull().sum()
+
+    mean_nutri_scores = (
+        data.groupby(["subcategory", "region_name"])["nutri_score_num"]
+        .mean()
+        .reset_index()
+    )
+
+    pivot_nutri_scores = mean_nutri_scores.pivot(
+        index="subcategory", columns="region_name", values="nutri_score_num"
+    )
+
+    category_mean_scores = pivot_nutri_scores.mean(axis=1).sort_values(ascending=False)
+    pivot_nutri_scores_sorted = pivot_nutri_scores.reindex(category_mean_scores.index)
+
+    pivot_nutri_scores_sorted = pivot_nutri_scores_sorted[
+        pivot_nutri_scores_sorted.max(axis=1) - pivot_nutri_scores_sorted.min(axis=1)
+        >= 0.5
+    ]
+
+    palette = sns.color_palette("tab10", n_colors=pivot_nutri_scores_sorted.shape[1])
+
+    pivot_nutri_scores_sorted.plot(
+        kind="barh",
+        stacked=False,
+        figsize=(15, 8),
+        color=palette,
+        edgecolor="white",
+        linewidth=0.5,
+        zorder=3,
+    )
+    plt.xlabel("Mean Nutri-Score [1:E, 2:D, 3:C, 4:B, 5:A]")
+    plt.ylabel("Sub Category")
+    plt.title(
+        "Mean Nutri-Score Comparison Across Regions for Each Sub Category (Difference >= 0.5)"
+    )
+    plt.legend(title="Region", bbox_to_anchor=(1.05, 1), loc="upper left")
+    plt.tight_layout()
+    plt.grid(axis="x", zorder=0)
+    plt.show()
+
+
+def nutri_subsubcat_histogram():
+    data = pd.read_csv(PATH + "/data/filtered_magasin_product.csv")
+
+    data[["subsubcategory", "region_name", "nutri_score_num"]].isnull().sum()
+
+    mean_nutri_scores = (
+        data.groupby(["subsubcategory", "region_name"])["nutri_score_num"]
+        .mean()
+        .reset_index()
+    )
+
+    pivot_nutri_scores = mean_nutri_scores.pivot(
+        index="subsubcategory", columns="region_name", values="nutri_score_num"
+    )
+
+    category_mean_scores = pivot_nutri_scores.mean(axis=1).sort_values(ascending=False)
+    pivot_nutri_scores_sorted = pivot_nutri_scores.reindex(category_mean_scores.index)
+
+    pivot_nutri_scores_sorted = pivot_nutri_scores_sorted[
+        pivot_nutri_scores_sorted.max(axis=1) - pivot_nutri_scores_sorted.min(axis=1)
+        >= 1
+    ]
+
+    palette = sns.color_palette("tab10", n_colors=pivot_nutri_scores_sorted.shape[1])
+
+    pivot_nutri_scores_sorted.plot(
+        kind="barh",
+        stacked=False,
+        figsize=(15, 8),
+        color=palette,
+        edgecolor="white",
+        linewidth=0.5,
+        zorder=3,
+    )
+    plt.xlabel("Mean Nutri-Score [1:E, 2:D, 3:C, 4:B, 5:A]")
+    plt.ylabel("Sub Sub Category")
+    plt.title(
+        "Mean Nutri-Score Comparison Across Regions for Each Sub Sub Category (Difference >= 0.5)"
+    )
+    plt.legend(title="Region", bbox_to_anchor=(1.05, 1), loc="upper left")
+    plt.tight_layout()
+    plt.grid(axis="x", zorder=0)
+    plt.show()
+
+
 def main():
     salary()
-    nutri_histogram()
+    nutri_cat_histogram()
+    nutri_subcat_histogram()
+    # nutri_subsubcat_histogram()
 
 
 if __name__ == "__main__":
